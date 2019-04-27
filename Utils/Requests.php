8ccad38708e6b3d489d0e97c4850ab2702d5ae8f
Douglas Silva <doug.hs@protonmail.ch>
2019-04-27 15:50:07 -0300

    Added functions that handle server requests such as POST, GET and the $_SERVER superglobal

--- Utils/Requests.php
+++ Utils/Requests.php
@@ -2,22 +2,37 @@
 
     interface Requests {
 
-        public static function isPost();
-        public static function get($value);
+        public static function method($method);
+        public static function self();
+        public static function get($method, $value);
 
     }
 
     class ServerRequest implements Requests {
 
-        public static function isPost() {
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        public static function method($method) {
+            if ($_SERVER["REQUEST_METHOD"] == $method) {
                 return TRUE;
             }
             return FALSE;
         }
 
-        public static function get($value) {
-            return $_POST[$value];
+        public static function self() {
+            return htmlspecialchars($_SERVER["PHP_SELF"]);
+        }
+
+        public static function get($method, $value) {
+            switch ($method) {
+                case 'post':
+                    return $_POST[$value];
+                    break;
+                case 'get':
+                    return $_GET[$value];
+                    break;
+                default:
+                    throw new Exception('Invalid request method');
+                    break;
+            }
         }
 
     }
