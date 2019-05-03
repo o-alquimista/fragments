c263325ee45abffc532f103b10a99d657f5d7f76
Douglas Silva <doug.hs@protonmail.ch>
2019-05-02 23:59:17 -0300

    Changes to how methods are called to optimize and reduce some lines

--- Controllers/Login.php
+++ Controllers/Login.php
@@ -19,52 +19,55 @@
     class LoginForm implements Login {
 
         /*
-        $feedbackText holds feedback messages and is retrieved
-        at the login view if the login() method returns FALSE
-        */
+         * $feedbackText holds feedback messages and is retrieved
+         * at the login view if the login() method returns FALSE
+         */
 
         public $feedbackText = array();
+        protected $connection;
 
         public function login($username, $passwd) {
 
+            /*
+             * We call the database connection class to
+             * return a connection object that we pass to the
+             * constructor of every class that requires it.
+             */
+
+            $Connect = new DatabaseConnection;
+            $this->connection = $Connect->getConnection();
+
             // Sanitize input
+
             $username = FilterInput::clean($username);
 
             // Returns FALSE if input validation fails
-            $FormValidation = new FormValidation;
-            $Validation = $FormValidation->validate($username, $passwd);
-            if ($Validation === FALSE) {
-                $this->feedbackText = $FormValidation->feedbackText;
+
+            $formValidation = new FormValidation;
+            if ($formValidation->validate($username, $passwd) === FALSE) {
+                $this->feedbackText = $formValidation->feedbackText;
                 return FALSE;
             }
 
-            /*
-            We create one instance of the database connection
-            class and, using dependency injection, we pass it
-            to the constructor of every class that needs it.
-            */
-
-            $connect = new DatabaseConnection;
-            $connection = $connect->getConnection();
-
             // Returns FALSE if user is not registered
-            $checkExists = new UserExists($connection);
-            $resultUserExists = $checkExists->isUserRegistered($username);
-            if ($resultUserExists === FALSE) {
+
+            $checkExists = new UserExists($this->connection);
+            if ($checkExists->isUserRegistered($username) === FALSE) {
                 $this->feedbackText[] = $checkExists->feedbackText;
                 return FALSE;
             }
 
             // Returns FALSE if password verification failed
-            $checkPassword = new PasswordVerify($connection);
-            $resultCheckPassword = $checkPassword->VerifyPassword($username, $passwd);
-            if ($resultCheckPassword === FALSE) {
+
+            $checkPassword = new PasswordVerify($this->connection);
+            if ($checkPassword->VerifyPassword($username, $passwd) === FALSE) {
                 $this->feedbackText[] = $checkPassword->feedbackText;
                 return FALSE;
             }
 
             // Authenticate user and return TRUE
-            $authentication = new Authenticate($connection);
+
+            $authentication = new Authenticate($this->connection);
             $authentication->setSessionVariables($username);
             return TRUE;
 
--- Controllers/Register.php
+++ Controllers/Register.php
@@ -19,48 +19,52 @@
     class Register implements Registration {
 
         /*
-        $feedbackText holds feedback messages and is retrieved
-        at the register view if the registerUser() method returns FALSE
+         * $feedbackText holds feedback messages and is retrieved
+         * at the register view if the registerUser() method returns FALSE
         */
 
         public $feedbackText = array();
+        protected $connection;
 
         public function registerUser($username, $passwd) {
 
+            /*
+             * We call the database connection class to
+             * return a connection object that we pass to the
+             * constructor of every class that requires it.
+             */
+            
+            $Connect = new DatabaseConnection;
+            $this->connection = $Connect->getConnection();
+
             // Sanitize input
+
             $username = FilterInput::clean($username);
 
             // Returns FALSE if input validation fails
+
             $FormValidation = new FormValidation;
-            $Validation = $FormValidation->validate($username, $passwd);
-            if ($Validation == FALSE) {
+            if ($FormValidation->validate($username, $passwd) === FALSE) {
                 $this->feedbackText = $FormValidation->feedbackText;
                 return FALSE;
             }
 
-            /*
-            We create one instance of the database connection
-            class and, using dependency injection, we pass it
-            to the constructor of every class that needs it.
-            */
-
-            $connect = new DatabaseConnection;
-            $connection = $connect->getConnection();
-
             // Returns FALSE if username is already registered
-            $UsernameAvailable = new UsernameAvailable($connection);
-            $resultUsernameAvailable = $UsernameAvailable->isUsernameAvailable($username);
-            if ($resultUsernameAvailable == FALSE) {
+
+            $UsernameAvailable = new UsernameAvailable($this->connection);
+            if ($UsernameAvailable->isUsernameAvailable($username) === FALSE) {
                 $this->feedbackText[] = $UsernameAvailable->feedbackText;
                 return FALSE;
             }
 
             // Hash the password
+
             $passwordHash = new PasswordHash;
             $hash = $passwordHash->hashPassword($passwd);
 
             // Write username and hash to the database
-            $writeData = new WriteData($connection);
+
+            $writeData = new WriteData($this->connection);
             $writeData->insertData($username, $hash);
             $this->feedbackText[] = $writeData->feedbackText;
 
