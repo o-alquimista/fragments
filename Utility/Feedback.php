c62dedf5d67c851fdb6deb69409126d23dbbbffe
Douglas Silva <doug.hs@protonmail.ch>
2019-05-16 16:38:45 -0300

    Each feedback type has their own classes and each of them hold their feedback messages (example: DangerFeedback has a property with 'danger' messages), extending from an abstract class which contains all the needed methods to retrieve and process feedback.

--- Utility/Feedback.php
+++ Utility/Feedback.php
@@ -10,81 +10,48 @@
 
 namespace Fragments\Utility\Feedback;
 
-use Fragments\Utility\Errors\SoftException;
+abstract class Feedback {
 
-interface FeedbackTools {
+    /*
+     * FIXME: if the feedbackID is invalid, throw a soft exception
+     * and pick a generic message explaining that something went wrong
+     * with the validation, then log the error.
+     */
 
-    public static function get($type, $feedback);
+    private $feedbackID;
 
-}
+    private $type;
 
-class Feedback implements FeedbackTools {
+    public function __construct($feedbackID) {
 
-    protected static $feedbackText = array(
-        'FEEDBACK_USERNAME_EMPTY' => 'Username was left empty',
-        'FEEDBACK_USERNAME_LENGTH' => 'Minimum username length is 4 characters',
-        'FEEDBACK_PASSWORD_EMPTY' => 'Password was left empty',
-        'FEEDBACK_PASSWORD_LENGTH' => 'Minimum password length is 8 characters',
-        'FEEDBACK_NOT_REGISTERED' => 'Invalid credentials',
-        'FEEDBACK_INCORRECT_PASSWD' => 'Invalid credentials',
-        'FEEDBACK_USERNAME_TAKEN' => 'Username already taken',
-        'FEEDBACK_REGISTRATION_COMPLETE' => 'Registration complete',
+        $this->feedbackID = $feedbackID;
 
-        'EXCEPTION_SESSION_EXPIRED' => 'This session has expired',
-    );
-
-    public static function get($type, $feedback) {
-
-        /*
-         * Method get() returns the requested feedback message
-         * formatted with the specified $type
-         */
-
-        $message = self::$feedbackText[$feedback];
-
-        try {
-
-            switch ($type) {
-                case 'warning':
-                    return self::format($type, $message);
-                    break;
-                case 'success':
-                    return self::format($type, $message);
-                    break;
-                case 'danger':
-                    return self::format($type, $message);
-                    break;
-                default:
-                    throw new SoftException($type);
-                    break;
-            }
-
-        } catch(SoftException $err) {
-
-            $err->invalidFeedbackType();
-
-            /*
-             * Default to 'secondary' when an invalid
-             * feedback type is detected
-             */
-
-            return self::format('secondary', $message);
-
-        }
+        $this->type = $this->getType();
 
     }
 
-    protected static function format($type, $feedback) {
+    public function get() {
 
-        /*
-         * Method format() inserts $feedback into the
-         * specified alert type and returns it
-         */
+        $message = $this->feedbackText[$this->feedbackID];
+
+        $message = $this->format($message);
+
+        return $message;
+
+    }
+
+    private function getType() {
+
+        return $this->feedbackType;
+
+    }
+
+    private function format($message) {
 
         ob_start();
 
-        echo "<div class='alert alert-" . $type . "'>
-            " . $feedback . "
+        echo "<div class='alert alert-" . $this->type . "'>
+            " . $message . "
             </div>";
 
         $output = ob_get_contents();
@@ -94,6 +61,42 @@ class Feedback implements FeedbackTools {
         return $output;
 
     }
+
+}
+
+class DangerFeedback extends Feedback {
+
+    protected $feedbackText = array(
+        'EXCEPTION_SESSION_EXPIRED' => 'This session has expired',
+    );
+
+    protected $feedbackType = 'danger';
+
+}
+
+class WarningFeedback extends Feedback {
+
+    protected $feedbackText = array(
+        'FEEDBACK_USERNAME_EMPTY' => 'Username was left empty',
+        'FEEDBACK_USERNAME_LENGTH' => 'Minimum username length is 4 characters',
+        'FEEDBACK_PASSWORD_EMPTY' => 'Password was left empty',
+        'FEEDBACK_PASSWORD_LENGTH' => 'Minimum password length is 8 characters',
+        'FEEDBACK_NOT_REGISTERED' => 'Invalid credentials',
+        'FEEDBACK_INCORRECT_PASSWD' => 'Invalid credentials',
+        'FEEDBACK_USERNAME_TAKEN' => 'Username already taken',
+    );
+
+    protected $feedbackType = 'warning';
+
+}
+
+class SuccessFeedback extends Feedback {
+
+    protected $feedbackText = array(
+        'FEEDBACK_REGISTRATION_COMPLETE' => 'Registration complete',
+    );
+
+    protected $feedbackType = 'success';
 
 }
 
